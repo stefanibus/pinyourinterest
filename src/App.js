bebfd2cd271b78e4f2034fa9180dabf0db409c63
Stefanibus <stefan.brueck@posteo.de>
2021-01-14 17:31:28 +0100

    initial setup, static api request, package react -router -dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import axios from "axios"; 
 import './App.css';
 
-function App() {
+ 
+
+export default function App() {
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://cdn.contentful.com/spaces/ifwqcmbkw16n/environments/master/entries?access_token=UtQ8Fkc_XdWNv24l0dq_QQAWVst5MZaGOAIKr6MvOf4&content_type=posts&fields.title[match]={klettern}"
+      )
+      .then((response) => {setCars(response.data.items) } )
+      .catch((error) => console.error(error));
+  }, []);
+  console.log(cars);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+  
+      {cars.map((iteration, index) => (
+        <div key={index}>
+          Car <img src={iteration.fields.image} />
+        </div>
+      ))}
+  
     </div>
   );
 }
+ 
 
-export default App;
+ 
