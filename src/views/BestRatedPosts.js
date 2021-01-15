be5f5c466ae91b6e1da1aa1bed49ae0358849a52
waldemar-bl <waldemar.blex@googlemail.com>
2021-01-15 18:00:31 +0100

    add view BestRatedPosts

---
+++ src/views/BestRatedPosts.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { useParam } from "react-router-dom";
+const BestRatedPosts = () => {
+  // const { Futureuse } = useParams();
+
+  return (
+    <div className="bestratedposts">
+      <div>irgendwas</div>
+      Hello-BestRatedPosts
+    </div>
+  );
+};
+export default BestRatedPosts;
