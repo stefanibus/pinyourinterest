1aa64af35b6b810177028813d0d20a589d6ae293
waldemar-bl <waldemar.blex@googlemail.com>
2021-01-18 21:49:15 +0100

    add component to App.js

---
+++ src/component/Formular.js
@@ -0,0 +1,28 @@
+import react from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+
+const Formular = () => {
+  return (
+    <div>
+      Hier ist das <span>Formular</span>
+      <Form inline>
+        {/* <Form.Label htmlFor="inlineFormInputName2" srOnly>
+          Name
+        </Form.Label> */}
+        <Form.Control
+          className="mb-2 mr-sm-2"
+          id="inlineFormInputName2"
+          placeholder="Search request"
+        />
+        <Button type="submit" className="mb-2">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Formular;
--- src/views/AllPosts.js
+++ src/views/AllPosts.js
@@ -5,15 +5,14 @@ const AllPosts = () => {
 
   return (
     <div className="allpots  container centerPage">
-     <div>
-      	<h1>All your Posts</h1>
-      	<div>
-      		To easily filter <b>'All your Posts'</b> <br/>
-      		please use the above Input-field.<br/>
-      		
-
-      	</div>
-      </div> 
+      <div>
+        <h1>All your Posts</h1>
+        <div>
+          To easily filter <b>'All your Posts'</b> <br />
+          please use the above Input-field.
+          <br />
+        </div>
+      </div>
     </div>
   );
 };
