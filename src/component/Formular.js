8fcb76ed8ca621826eb8ba4716e10b42d5b8e210
waldemar-bl <waldemar.blex@googlemail.com>
2021-01-18 21:52:56 +0100

    delete inputgroup from react bootstrap import

--- src/component/Formular.js
+++ src/component/Formular.js
@@ -1,7 +1,6 @@
 import react from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 
 const Formular = () => {
