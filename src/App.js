3b1e9fef11a58b478c27def7cebfb0173503c6b2
Stefanibus <stefan.brueck@posteo.de>
2021-01-20 16:40:56 +0100

    API-Request GetAllPosts

--- src/App.js
+++ src/App.js
@@ -13,96 +13,62 @@ export default function App() {
     const [userData, setUserData] = useState([]);
     const [postsDataForUsers, setPostsDataForUsers] = useState([]);
 
-  
+
+
+
+
+        const getAllPost = async () => {
+            try {
+              const getUserData = await axios.get(
+                'https://cdn.contentful.com/spaces/ifwqcmbkw16n/environments/master/entries?access_token=UtQ8Fkc_XdWNv24l0dq_QQAWVst5MZaGOAIKr6MvOf4&content_type=users'
+              );
+              const userIds = getUserData.data.items.map(iteration => {
+                return iteration.sys.id;
+              });
+              const getURLDynamic =
+                'https://cdn.contentful.com/spaces/ifwqcmbkw16n/environments/master/entries?access_token=UtQ8Fkc_XdWNv24l0dq_QQAWVst5MZaGOAIKr6MvOf4&content_type=posts&fields.userref.sys.id[in]=' +
+                userIds;
+              const callPosts = await axios.get(getURLDynamic);
+              setUserData(getUserData);
+              setPostsDataForUsers(callPosts.data.items);
+            } catch (err) {
+              console.error(err);
+            }
+          };
+
+
+    
+    const logFunction = () => {
+        console.log('postsDataForUsers   )): ');
+        console.log(postsDataForUsers);
+
+        console.log('postsDataForUsers  ');
+        console.log(postsDataForUsers);
+        console.log('postsDataForUsers[0].fields.userref.sys.id   ');
+        console.log(postsDataForUsers[0].fields.userref.sys.id); // UserREferenzID
+        console.log(postsDataForUsers[0].fields.description); // Description
+        console.log(postsDataForUsers[0].fields.title); // Title
+        console.log(postsDataForUsers[0].fields.rating); // Rating
+        console.log(postsDataForUsers[0].fields.webimage); // Webimage
+      };
  
 
 
-    const getAllPostsFromUser = async () => {
-        let getURLStatic = "https://cdn.contentful.com/spaces/ifwqcmbkw16n/environments/master/entries?access_token=UtQ8Fkc_XdWNv24l0dq_QQAWVst5MZaGOAIKr6MvOf4&content_type=posts&fields.userref.sys.id[all]=14U1y0dzbaaqOkvjr9kW4T,4CrLLnhKykpOV9fu2v7Odg,7gusLBhWbTDHN2ax00HnEk,6eCJKi4fMxQPQZCBRUF1dh,YrNbyp3ac9ibMecerzmv1" ;
- 
-        const callPosts = await axios
-            .get(
-                getURLStatic
-            )
-            // .then((response) => {
-            //     return (response);
-            // })
-            // .catch((error) => console.error(error));
-
-        console.log('callPosts', callPosts);
-        return callPosts;
- 
-    }
- 
 
 
-    const logFunction = () => { 
-        // console.log('postsDataForUsers   )): ');
-        // console.log(postsDataForUsers); 
-        console.log('callPosts   )): ');
-        console.log(callPosts); 
 
-        // console.log('postsDataForUsers.data.items (all POSTS are 21 Items?!   ANSCHEINEND ALLE USER  )): ');
-        // console.log(postsDataForUsers.data.items); 
-        // console.log('postsDataForUsers.data.items[0].fields.userref.sys.id   ANSCHEINEND ALLE USER  )): ');
-        // console.log(postsDataForUsers.data.items[0].fields.userref.sys.id);  // UserREferenzID
-        // console.log(postsDataForUsers.data.items[0].fields.description);  // Description 
-        // console.log(postsDataForUsers.data.items[0].fields.title);  // Title
-        // console.log(postsDataForUsers.data.items[0].fields.rating); // Rating 
-        // console.log(postsDataForUsers.data.items[0].fields.webimage); // Webimage  
-    }
- 
-
-      useEffect(() => {   
-
-
-        // WHY DOES THIS SAY undefined if I put this line inside of useEffect?! 
-        // const myUserArray = getAllUserData();
-
-     
-        // const asyncWork1 = async  () =>  {
-        // const DataUsers = await  getAllUserData(); 
-        // } 
-        // //
-        // asyncWork1();
-  
-        // const asyncWork2 = async () =>  { 
-        // const ourArrayOfUserIDs = await getAllPostsFromUser(DataUsers);
-   
-        // console.log('ourArrayOfUserIDs:  ');
-        // console.log(ourArrayOfUserIDs);
-        // } 
-        // //
-        // asyncWork2();
- 
-
+      useEffect(() => {    
+          getAllPost();  
      }, []);
 
   
 
     return (
-        <div className="App">
-         
-        {/*  <button onClick={ () => { console.log('getArrayOfUserIDs(getAllUserData)'); getArrayOfUserIDs(getAllUserData); }} >  getArrayOfUserIDs(getAllUserData)  </button>  <br/>
-        */}
+        <div className="App">  
 
-   {/*       <button onClick={ () => { console.log('getAllUserData()'); getAllUserData(); }} >  getAllUserData()  </button>  <br/>
-    */}
-       {/*   <button onClick={ () => { console.log('myUserArray');  console.log(myUserArray);   }} > console.log('myUserArray');  </button>  <br/>
-    */}
-         {/* <button onClick={ () => { console.log('getArrayOfUserIDs(getUserData)'); getArrayOfUserIDs(getUserData); }} >  getArrayOfUserIDs(getAllUserData)  </button>  <br/>
-    */}
-    
-          <button onClick={ () => { console.log('getAllPostsFromUser()'); getAllPostsFromUser(); }} >  getAllPostsFromUser() </button>  <br/>
-
-          <button onClick={ () => { 
-            console.log('logFunction()'); 
-            // logFunction(); 
-            }} 
-            >  logFunction() </button>  <br/>
-
-            
-
+      <button onClick={ () => { console.log('logFunction()'); logFunction(); }} >  logFunction() !</button>  <br/>
+  
+ 
       <NavBar/> 
     
       <Switch>
@@ -123,3 +89,7 @@ export default function App() {
     </div>
     );
 }
+
+
+
+
