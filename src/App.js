8b7b97d186c8cd95c38cc05d7ac4f6b5fa5323f3
Stefanibus <stefan.brueck@posteo.de>
2021-01-21 23:09:46 +0100

    added user-menu-dropdown, technically working, dropdown with npm select still missing

--- src/App.js
+++ src/App.js
@@ -7,41 +7,51 @@ import BestRatedPosts from "./views/BestRatedPosts";
 import PageNotFound from "./views/404";
 import NavBar from "./components/NavBar";
 import Formular from "./components/Formular";
+import Dropdown from "./components/Dropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-export default function App() {
-  const [userData, setUserData] = useState([]);
+export default function App() { 
   const [postsDataForUsers, setPostsDataForUsers] = useState();
+ 
 
-  const getAllPost = async () => {
+  const getAllPost = async (passValTest) => { 
     try {
       const getUserData = await axios.get(
         "https://cdn.contentful.com/spaces/ifwqcmbkw16n/environments/master/entries?access_token=UtQ8Fkc_XdWNv24l0dq_QQAWVst5MZaGOAIKr6MvOf4&content_type=users"
       );
       const userIds = getUserData.data.items.map((iteration) => {
-        return iteration.sys.id;
+
+        if (passValTest) {
+          // console.log('passValTest from inside of the  axios call ');
+          // console.log(passValTest);
+          return passValTest;          
+        } 
+          // console.log('pass iteration.sys.id from inside of the  axios call '); 
+          return iteration.sys.id; 
       });
       const getURLDynamic =
         "https://cdn.contentful.com/spaces/ifwqcmbkw16n/environments/master/entries?access_token=UtQ8Fkc_XdWNv24l0dq_QQAWVst5MZaGOAIKr6MvOf4&content_type=posts&fields.userref.sys.id[in]=" +
         userIds;
       const callPosts = await axios.get(getURLDynamic);
-      setUserData(getUserData);
+ 
       setPostsDataForUsers(callPosts);
     } catch (err) {
       console.error(err);
     }
   };
-
+ 
 
   useEffect(() => {
-    getAllPost();
+    getAllPost(); 
   }, []);
 
   return (
     <div className="App"> 
       <NavBar />
       <Formular />
+      <Dropdown userArray={postsDataForUsers} triggering={ (data) => {  
+        getAllPost(data)} }  />
       <Switch>
         <Route path="/allposts">
           <AllPosts postsDataForUsers={postsDataForUsers} />
