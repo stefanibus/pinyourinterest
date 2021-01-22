0d70cb0c628b6646c0fcc5aa22b8879051cf801f
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-01-22 09:57:43 +0100

    fix indentation and warnings

--- src/components/PostGrid.js
+++ src/components/PostGrid.js
@@ -1,31 +1,30 @@
 import CardColumns from "react-bootstrap/CardColumns";
 import Post from "./Post";
- 
-const PostGrid = ( {listPosts} ) => {
- 
-		const allPosts = listPosts ? listPosts.data.items.map((post, index)  => {
 
-	      	// map-function to compare userArrayID with userID  
-			const userID = post.fields.userref.sys.id 
-			const userArray = listPosts.data.includes.Entry   
-			userArray.map((i, index) => { 
-				// find matching ID Values 
-				if (i.sys.id == userID) { 
-					// add relevant user Data to the post-iteration 
-					post.user = i.fields 
-				}  
-			})   
-			return  <Post post={post} key={index} />  
-		}) : []
- 
-   return(
-   	<> 
-   	<CardColumns>{allPosts}</CardColumns>
-	</> 
-   	); 
+const PostGrid = ( {listPosts} ) => {
+    /* Convert fetched post data list to JSX items */
+    const allPosts = listPosts 
+      ? listPosts.data.items.map((post, index)  => {
+        // compare userArrayID with userID  
+        const userID = post.fields.userref.sys.id 
+        const userArray = listPosts.data.includes.Entry   
+        /* FIXME: "Array.prototype.map() expects a return value from arrow function. [13, 34]"*/
+        userArray.map((i, index) => { 
+          // find matching ID Values 
+          if (i.sys.id.toString() === userID.toString()) { 
+            // add relevant user Data to the post-iteration 
+            post.user = i.fields 
+          } 
+        })   
+        return  <Post post={post} key={index} />  
+      }) 
+      : []
+
+    return(
+      <> 
+        <CardColumns>{allPosts}</CardColumns>
+      </> 
+    ); 
 };
 
-export default PostGrid;
-
- 
- 
+export default PostGrid;  
